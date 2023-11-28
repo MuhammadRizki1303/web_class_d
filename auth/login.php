7088ef786bb3018f655f36b42e12f1db9f10da93
rizki <112327133+MuhammadRizki1303@users.noreply.github.com>
2023-11-28 16:57:10 +0700

    penyempurnaan login yang insya allah fixx

--- auth/login.php
+++ auth/login.php
@@ -59,19 +59,16 @@
                                 $password = $_POST["password"];
 
                                 // Gantilah ini dengan kredensial pengguna aktual Anda
+                                $id = 1;
                                 $adminUser = "admin@gmail.com";
                                 $adminPassword = "admin123";
-
-                                if ($email == $adminUser && $password == $adminPassword) {
+                                $sql = "SELECT * FROM login WHERE id='$id'";
+                                $result = $conn->query($sql);
+                                if ($result->num_rows > 0) {
                                     $_SESSION["user"] = "admin";
-                                    $date = date("Y-m-d H:i:s");
-                                    $sql = "INSERT INTO login (user, pass, tgl_isi) VALUES ('$email', '$password', '$date')";
-                                    if ($conn->query($sql) === TRUE) {
                                     header("Location: ../admin/dashboard.php");
                                     exit();
-                                } else {
-                                    echo "Error: " . $sql . "<br>" . $conn->error;
-                                }
+
                                 } else {
                                     echo "<p class='text-danger'>Username atau password salah</p>";
                                 }
