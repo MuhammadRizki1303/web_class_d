666c6f5b36f5866734eeec3dc9f8b1aca0f636c6
rizki <112327133+MuhammadRizki1303@users.noreply.github.com>
2023-11-28 17:02:37 +0700

    penyempurnaan login fixx

--- auth/login.php
+++ auth/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kelas TI-1D</title>
+    <title>Kelas TI Class D</title>
 
     <link rel="shortcut icon" href="assets/img/tid.png" type="image/x-icon">
 
@@ -68,7 +68,6 @@
                                     $_SESSION["user"] = "admin";
                                     header("Location: ../admin/dashboard.php");
                                     exit();
-
                                 } else {
                                     echo "<p class='text-danger'>Username atau password salah</p>";
                                 }
